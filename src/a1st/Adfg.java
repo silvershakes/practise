4b83a0b49cce1cca2b8da6cca54708c237d1ee44
silvershakes <pravinbalgore2850@gmail.com>
2022-05-28 11:06:01 +0530

    apply

---
+++ src/a1st/Adfg.java
@@ -0,0 +1,30 @@
+package a1st;
+
+public class Adfg {
+
+	public static void main(String[] args)
+	{
+//	      *****
+//	      ****
+//	      ***
+//	      **
+//	      *
+		
+		
+		
+		int star=5;
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=star;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			star--;
+		}
+		
+
+	}
+
+}
