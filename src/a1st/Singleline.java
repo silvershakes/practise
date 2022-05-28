5977f295bf9ec8fbb812b5eeed2712d906bab9ea
silvershakes <pravinbalgore2850@gmail.com>
2022-05-28 11:10:53 +0530

    apply0

---
+++ src/a1st/Singleline.java
@@ -0,0 +1,26 @@
+package a1st;
+
+public class Singleline {
+
+	public static void main(String[] args) {
+		//*****************
+		
+		for(int i=1;i<=20;i++)
+		{
+			System.out.print(" *");
+		}
+		
+//		*
+//		*
+//		*
+		
+		for(int i=1;i<=20;i++)
+		{
+			System.out.println("*");
+		}
+		
+		
+		
+	}
+
+}
